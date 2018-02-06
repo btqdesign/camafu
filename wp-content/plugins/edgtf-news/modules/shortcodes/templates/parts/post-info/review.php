962b6a1fcf68d094922de64e7d28bd67ebe5300d
idevol <saul.diaz.v@gmail.com>
2018-02-06 00:48:10 -0600

    Edge News

--- wp-content/plugins/edgtf-news/modules/shortcodes/templates/parts/post-info/review.php
+++ wp-content/plugins/edgtf-news/modules/shortcodes/templates/parts/post-info/review.php
@@ -1,6 +1,6 @@
 <?php
 $display_review                 = isset( $display_review ) && $display_review !== '' ? $display_review : 'yes';
-$review_average                 = get_post_meta( get_the_ID(), 'edgtf_news_review_average', true );
+$review_average                 = floatval(get_post_meta( get_the_ID(), 'edgtf_news_review_average', true ));
 $review_average_params['style'] = 'width: ' . ( $review_average * 20 ) . '%'; //20 is 100/5, calculating percent
 
 if ( $display_review == 'yes' && $review_average !== '' ) { ?>
