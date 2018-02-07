a98211041fb44a7e15feb145adabc247fcf06eff
Saul <saul.diaz.v@gmail.com>
2018-02-06 18:58:33 -0600

    Diccionario correcto

--- wp-content/plugins/edgtf-news/modules/shortcodes/templates/parts/post-info/author.php
+++ wp-content/plugins/edgtf-news/modules/shortcodes/templates/parts/post-info/author.php
@@ -3,7 +3,7 @@ $display_author = isset( $display_author ) && $display_author !== '' ? $display_
 
 if ( $display_author == 'yes' ) { ?>
 	<div class="edgtf-post-info-author">
-		<span class="edgtf-post-info-author-text"><?php esc_html_e( 'by', 'edgtf-news' ); ?></span>
+		<span class="edgtf-post-info-author-text"><?php esc_html_e( 'by', 'mediadesk' ); ?></span>
 		<a itemprop="author" class="edgtf-post-info-author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
 			<?php the_author_meta( 'display_name' ); ?>
 		</a>
